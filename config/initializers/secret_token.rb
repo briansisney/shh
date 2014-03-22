5654935b0b03d7fa6f591780fe01a45ac8490c67
Brian Sisney <briansisney@gmail.com>
2014-03-22 18:29:27 -0400

    installed mongoid gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shhnogames::Application.config.secret_key_base = 'db4d3e3f0b9d60c8862d9427c9710a8d4950086a6ec42a4d5d50f559d80dac190f666b43950117d0a3a1e7d66cbe99f76254e2723117ffe1ed0257addc24d40b'
